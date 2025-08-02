64aa0a0e015c01320e3ad0e0efa9d50e99162df2
lrrysln <larry11sloan@me.com>
2025-08-01 21:50:53 -0400

    Update app.py

--- weekly-report-app/app.py
+++ weekly-report-app/app.py
@@ -158,5 +158,3 @@ if password:
         if html_report:
             st.markdown(html_report, unsafe_allow_html=True)
 
-st.write(df[["Store Number", "Timestamp", "Delta Days", "Trend"]].sort_values(["Store Number", "Timestamp"]).head(20))
-
